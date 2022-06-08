aa7bdd6ab3a57efc55c8e7cbc0c4a23f2fb55ee9
Benci Magpoc <BENCIIAN@GMAIL.COM>
2022-06-08 11:17:38 -0400

    added methods to roles service

--- BugTrackerByBenci/Services/Interfaces/IRolesService.cs
+++ BugTrackerByBenci/Services/Interfaces/IRolesService.cs
@@ -5,9 +5,15 @@ namespace BugTrackerByBenci.Services.Interfaces
 {
     public interface IRolesService
     {
-        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+        public Task<List<IdentityRole>> GetBTRolesAsync();    
+        public Task<string> GetRoleNameByIdAsync(string roleId);
         public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
-        public string GetUserRoleNameAsync(BTUser user);
-
+        public Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId);
+        public Task<string> GetUserRoleAsync(BTUser user);
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+        public Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles);
     }
 }
--- BugTrackerByBenci/Services/RolesService.cs
+++ BugTrackerByBenci/Services/RolesService.cs
@@ -19,6 +19,48 @@ namespace BugTrackerByBenci.Services
             _userManager = userManager;
         }
 
+        public async Task<bool> AddUserToRoleAsync(BTUser user, string roleName)
+        {
+            try
+            {
+                bool result = (await _userManager.AddToRoleAsync(user, roleName)).Succeeded;
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<IdentityRole>> GetBTRolesAsync()
+        {
+            try
+            {
+                List<IdentityRole> result = new();
+                result = await _context.Roles.ToListAsync();
+                return result!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<string> GetRoleNameByIdAsync(string roleId)
+        {
+            try
+            {
+                IdentityRole? role = _context.Roles.Find(roleId);
+                string result = await _roleManager.GetRoleNameAsync(role!);
+
+                return result!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public async Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId)
         {
             try
@@ -33,6 +75,51 @@ namespace BugTrackerByBenci.Services
                 throw;
             }
         }
+
+        public async Task<List<BTUser>> GetUsersNotInRoleAsync(string roleName, int companyId)
+        {
+            try
+            {
+                List<string> userIds =
+                    (await _userManager.GetUsersInRoleAsync(roleName)).Select(u => u.Id).ToList();
+                List<BTUser> roleUsers = _context.Users.Where(u => !userIds.Contains(u.Id)).ToList();
+
+                List<BTUser> result = roleUsers.Where(u => u.CompanyId == companyId).ToList();
+                
+                return result!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<string> GetUserRoleAsync(BTUser user)
+        {
+            try
+            {
+                IEnumerable<string> result = await _userManager.GetRolesAsync(user);
+                return result.FirstOrDefault()!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<IEnumerable<string>> GetUserRolesAsync(BTUser user)
+        {
+            try
+            {
+                IEnumerable<string> userRoles = await _userManager.GetRolesAsync(user);
+                return userRoles;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public async Task<bool> IsUserInRoleAsync(BTUser user, string roleName)
         {
             try
@@ -48,14 +135,32 @@ namespace BugTrackerByBenci.Services
             }
         }
 
-        public string GetUserRoleNameAsync(BTUser user)
+        public async Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName)
         {
             try
             {
-                var roleId = _context.UserRoles.First(u=>u.UserId==user.Id).RoleId;
-                var roleName = _context.Roles.Find(roleId)!.Name;
-                
-                return roleName!;
+                bool result = (await _userManager.RemoveFromRoleAsync(user, roleName)).Succeeded;
+                return result!;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> RemoveUserFromRolesAsync(BTUser user, IEnumerable<string> roles)
+        {
+            try
+            {
+                try
+                {
+                    bool result = (await _userManager.RemoveFromRolesAsync(user, roles)).Succeeded;
+                    return result!;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
             }
             catch (Exception)
             {
